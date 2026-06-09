c913e158d6c8171a187f1804e4445654254cb0fa
Michael Denyer <michael.denyer@effem.com>
2026-06-09 23:38:34 +0100

    feat: scaffold commit-cafe package

---
+++ src/commit_cafe/__init__.py
@@ -0,0 +1,3 @@
+"""Commit Cafe: render GitHub activity as an animated cat-cafe SVG."""
+
+__version__ = "0.1.0"
---
+++ tests/test_state.py
@@ -0,0 +1,5 @@
+from commit_cafe import __version__
+
+
+def test_package_imports():
+    assert __version__ == "0.1.0"
